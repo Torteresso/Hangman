b70865efb315827c01af078c2566d5a7d71a651e
torto <torte.torto@gmail.com>
2024-12-09 09:40:54 +0100

    added fundamental methods in hanglan class

--- hangman.cpp
+++ hangman.cpp
@@ -12,29 +12,76 @@ void Hangman::generateWord()
 		m_word.push_back({ letter });
 	}
 
-	m_plumesLeft = m_nbPlumes;
+	m_GuessesLeft = m_nbGuesses;
 	m_lettersEntered.clear();
 }
 
 void Hangman::sayRules()
 {
 	std::cout << "Welcome to Hangman !\n";
-	std::cout << "To win: guess the word. To lose: run out of plumes.\n";
+	std::cout << "To win: guess the word. To lose: run out of plumes.\n\n";
 }
 
 void Hangman::displayWord()
 {
 	std::cout << "The word:";
-	m_lettersEntered.
+	
+	for (const auto& letter : m_word)
+	{
+		if (isLetterInArray(letter, m_lettersEntered))
+		{
+			std::cout << letter;
+		}
+		else
+		{
+			std::cout << '_';
+		}
+	}
+
 }
 
-void Hangman::letterInWord(const char c)
+void Hangman::displayGuesses()
 {
+	std::cout << "\tWrong guesses: ";
+	
+	for (int i{}; i < m_nbGuesses; i++)
+	{
+		for (const auto& letter : m_lettersEntered)
+		{
+			if (isLetterInArray(letter, m_word)) {}
 
+
+		}
+	}
+}
+
+void Hangman::displayTurn()
+{
+	displayWord();
+}
+
+bool Hangman::isLetterInArray(const char c, const std::vector<char>& arr)
+{
+	for (const auto& letter : arr)
+	{
+		if (c == letter)
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
 
 void Hangman::play()
 {
-	sayRules();
-	generateWord();
+	/*do
+	{*/
+		sayRules();
+		generateWord();
+
+		displayTurn();
+
+	//} while (m_plumesLeft > 0);
+	
 }
--- hangman.h
+++ hangman.h
@@ -6,16 +6,19 @@
 class Hangman
 {
 private:
-	const int m_nbPlumes{ 6 };
+	const int m_nbGuesses{ 6 };
 
-	int m_plumesLeft{ m_nbPlumes };
+	int m_GuessesLeft{ m_nbGuesses };
 	std::vector<char> m_word{};
 	std::vector<char> m_lettersEntered{};
 
 	void generateWord();
 	void sayRules();
 	void displayWord();
-	bool letterInWord(const char c);
+	bool isLetterInArray(const char c, const std::vector<char>& arr);
+	void displayTurn();
+	void displayGuesses();
+
 
 public:
 	void play();
